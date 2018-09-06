2d18615a13c58945015c5db3c50057e4933a695a
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-06 16:24:57 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,22 +12,27 @@ def first_element(rico)
   my_first_element = rico[0]
   return my_first_element
 end
+ 
  def third_element (rico)
   my_third_element = rico[2]
   return my_third_element
 end
+ 
  def last_element (rico)
   my_last_element = rico[-1]
   return my_last_element
 end
+ 
  def first_element_with_array_methods(soup)
   chicken = soup.first 
   return chicken
 end
+ 
  def last_element_with_array_methods(stew)
   pumpkin = stew.last 
   return stew
 end
+ 
  def length_of_array(languages)
   length = languages.count 
   return length 
